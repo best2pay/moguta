d440347c0fc24360b8b187f6a39dc34844c41edb
best2pay <github@best2pay.net>
2022-12-13 12:08:12 +0300

    fix purchase_by_qr

--- best2pay/best2pay-payment/best2payPayment.php
+++ best2pay/best2pay-payment/best2payPayment.php
@@ -76,7 +76,7 @@ function orderWasPayed($response, $password) {
     if ($order_id == 0)
         return false;
     // check payment state
-    if (($response['type'] == 'PURCHASE_BY_QR' || $response['type'] == 'PURCHASE' || $response['type'] == 'AUTHORIZE') && $response['state'] == 'APPROVED')
+    if (($response->type != 'PURCHASE_BY_QR' && $response->type != 'PURCHASE' && $response->type != 'AUTHORIZE') || $response->state != 'APPROVED')
         return false;
     // check server signature
     $tmp_response = json_decode(json_encode($response), true);
